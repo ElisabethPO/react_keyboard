41e9fd4621c1f0f82fc1d07f58b1bde07fe2553d
Elisabeth Polska <polskaelizaveta@gmail.com>
2025-07-15 09:39:10 +0300

    add task solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
-import { App } from './App';
+import { App } from './components/App';
 
 import './index.scss';
 
 createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+
